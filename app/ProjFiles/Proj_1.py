7062b1c18552d9bf2af6838e4a8a479a74b92f68
VladVons <vladvons@gmail.com>
2022-04-27 09:15:11 +0300

    MyCommit

--- app/ProjFiles/Proj_1.py
+++ app/ProjFiles/Proj_1.py
@@ -4,14 +4,10 @@ from ProjFiles import TProjFiles
 
 def Main1():
     PF = TProjFiles('../src')
-    PF.FilesLoad(['vRelaySrv.py'])
+    PF.FilesLoad(['vRelaySrv.py', 'App/Scraper/__init__.py', 'App/ScraperSrv/__init__.py'])
     PF.DirsLoad(['Conf'])
-
     PF.DirsLoad(['App/WebSrv'], True)
-    PF.FilesLoad(['App/Scraper/__init__.py', 'App/ScraperSrv/__init__.py'])
-
     PF.Release('Proj_1')
-
 
 os.system('clear')
 Main1()
