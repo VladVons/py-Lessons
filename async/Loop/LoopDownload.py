06a8be337f2a0b4ca4ad3112129c929487aa1d96
VladVons <vladvons@gmail.com>
2022-05-29 08:08:08 +0300

    MyCommit

--- async/Loop/LoopDownload.py
+++ async/Loop/LoopDownload.py
@@ -19,31 +19,32 @@ async def TaskA(aSleep: int):
         print('TaskA(%s) loop' % aSleep, Counter)
         await asyncio.sleep(aSleep)
 
-async def TaskB(aSleep: int, aType: str):
+async def TaskB(aType: str):
     Url = 'https://loremflickr.com/800/600/%s' % aType
     Counter = 0
     while True:
+        try:
+            async with aiohttp.ClientSession() as Session:
+                async with Session.get(Url) as Response:
+                    Data = await Response.read()
+        except Exception as E:
+            print('Error', E)
+            break 
+            
         Counter += 1
-        async with aiohttp.ClientSession() as Session:
-            async with Session.get(Url) as Response:
-                Data = await Response.read()
-                File = 'image_%s_%03d.jpg' % (aType, Counter)
-                print('Save', Url, File)
-                with open(File , 'wb') as F:
-                    F.write(Data)
-        await asyncio.sleep(aSleep)
+        File = 'image_%s_%03d.jpg' % (aType, Counter)
+        print('Save', Url, File)
+        with open(File , 'wb') as F:
+            F.write(Data)
+        await asyncio.sleep(1)
 
 
-async def Main(aMaxTasks: int):
-    ImageType = ['dog', 'cat', 'girl', 'boy', 'tree']
-    if (aMaxTasks >= len(ImageType)):
-        print('Maximum %s value allowed' % len(ImageType))
-        return
-
+async def Main():
+    ImageType = ['python', 'dog', 'cat', 'girl', 'boy', 'tree', 'beer', 'smile', 'bird', 'war', 'moon', 'sun', 'star']
     Tasks = []
-    for i in range(aMaxTasks):
+    for i in range(len(ImageType)):
         Tasks.append(asyncio.create_task(TaskA(i+1)))
-        Tasks.append(asyncio.create_task(TaskB(i+1, ImageType[i])))
+        Tasks.append(asyncio.create_task(TaskB(ImageType[i])))
     await asyncio.gather(*Tasks)
 
-asyncio.run(Main(4))
+asyncio.run(Main())
