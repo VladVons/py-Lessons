7d661cea23808a848b947fa2efc696f12e84391e
VladVons <vladvons@gmail.com>
2022-07-21 20:25:10 +0300

    MyCommit

--- tasks/01/task_48.py
+++ tasks/01/task_48.py
@@ -3,5 +3,5 @@ convert list to paired list
 ListA = [1, 2, 3, 4, 5, 6, 7, 8]
 
 result example
-[[1, 2], [3, 4], [5, 6], [7 ,8]]
+[[1, 2], [3, 4], [5, 6], [7, 8]]
 '''
---
+++ tasks/01/task_54.py
@@ -0,0 +1,7 @@
+'''
+find digits that contains 3 
+ListA = [1, 12, 321, 245, 643, 3, 17, 33]
+
+result example
+[321, 643, 3, 33]
+'''
