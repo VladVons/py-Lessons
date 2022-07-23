48a621a3f03112dae6d74611829e359f87bf9ce9
VladVons <vladvons@gmail.com>
2022-07-23 08:58:14 +0300

    MyCommit

---
+++ tasks/01/task_58.py
@@ -0,0 +1,12 @@
+'''
+count vowel letters in each word
+ListA = ['count', 'vowel', 'letters', 'in', 'each', 'word']
+
+result example
+count - 2
+vowel - 2
+letters - 2
+in - 1
+each -2
+word- 1
+'''
---
+++ tasks/01/task_59.py
@@ -0,0 +1,8 @@
+'''
+find abnormal sequence value and its index
+ListA = [1, 2, 3, 4, 1, 6, 7, 8, 2]
+
+result example
+4 - 1
+2 - 8
+'''
