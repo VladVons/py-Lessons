334814c19c3b5f685b936bfdb94da838f4ecf39c
vladvons <vladvons@gmail.com>
2022-10-31 21:46:00 +0200

    MyCommit

--- app/ProjFiles/Proj_vB2B/Task/WebSrv/form/info.py
+++
@@ -1,9 +0,0 @@
-# Created: 2022.03.14
-# Author: Vladimir Vons <VladVons@gmail.com>
-# License: GNU, see LICENSE for more details
-
-
-from .FormBase import TFormBase
-
-class TForm(TFormBase):
-    Title = 'Information'
--- app/ProjFiles/Proj_vB2B/vB2B.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python3 -V
-python3 -B vB2B.py -s
