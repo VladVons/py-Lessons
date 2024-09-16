7947a96c0e97c9bc54005ee103e0c2a0cd3bbc70
VladVons <vladvons@gmail.com>
2024-09-16 16:42:48 +0300

    MyCommit

--- app/ProjFiles/vCrawler/Proj_vCrawler/IncP/LibCrawler.py
+++
@@ -1,9 +0,0 @@
-# Created: 2024.04.07
-# Author:  Vladimir Vons <VladVons@gmail.com>
-# License: GNU, see LICENSE for more details
-
-
-# pylint: skip-file
-from Inc.DbList import TDbList, TDbSql, TDbRec
-from Inc.Util.Obj import DeepGetByList, GetDictDef, GetDictDefs, Filter, DeepGetsRe, Iif, IsDigits
-from .Log import Log
--- app/ProjFiles/vCrawler/Proj_vCrawler/vCrawler.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-py=python3.12
-Dir=~/VirtEnv/$py
-source $Dir/bin/activate
-
-$py -B vCrawler.py
